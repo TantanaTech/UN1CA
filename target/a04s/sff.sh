d7f370cb0e759de0cd88e2bbf851029ace80b2fb
TantanaTech <yorumdeneme4@gmail.com>
2026-01-04 16:21:25 +0300

    Create sff.sh

---
+++ target/a04s/sff.sh
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2025 Salvo Giangreco
+#
+# Licensed under the GNU General Public License, Version 3.0
+#
+
+# SEC Floating Feature configuration file for Galaxy A04s (a04s)
+
+# Enable refresh rate feature (A04s supports 90Hz)
+# Mode 1 genellikle 90Hz veya standart yüksek tazeleme hızı için kullanılır.
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=1
+
+# Enable extra brightness feature
+# A04s paneli destekliyorsa açık kalabilir, batarya tasarrufu için TRUE bıraktım.
+SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
+
+# AI Revital Version (Gallery Remaster)
+# A04s işlemcisi (Exynos 850) için daha hafif bir sürüm olan 1.5 daha stabildir.
+SEC_FLOATING_FEATURE_SAIV_CONFIG_AI_REVITAL_VERSION=1.5,1
+
+# A04s Özel: Yüz tanıma ve parmak izi hızlandırma (Opsiyonel)
+SEC_FLOATING_FEATURE_BIO_CONFIG_BIOMETRICS_FRAMEWORK_VERSION=2.0
